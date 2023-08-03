e6871d97957c6d32fc59577a1e3f9bafd82b6820
Lucas <admlucasvieira@gmail.com>
2023-08-03 02:21:00 -0300

    cart test development

--- tests/cart-test.spec.js
+++ tests/cart-test.spec.js
@@ -6,19 +6,32 @@ test('Browser Context Test', async ({ browser }) => {
 
     // Product 1 selection
 
-    const firstItem = 'a[href="prod.html?idp_=1"].hrefch';
-    await newTab.locator(firstItem).click();
+    const firstItemLocator = 'a[href="prod.html?idp_=1"].hrefch';
+    const firstItemName = await newTab.locator(firstItemLocator).textContent();
+    console.log(firstItemName);
+    await newTab.locator(firstItemLocator).click();
     await newTab.locator(".btn.btn-success.btn-lg").click();
     await newTab.locator("[id='nava']").click();
+
+    // Product 2 menu navigation
 
     const submenuSelector = 'a#itemc[onclick="byCat(\'notebook\')"].list-group-item';
     await newTab.locator(submenuSelector).click();
 
-    const secondItem = 'a[href="prod.html?idp_=8"].hrefch';
-    await newTab.locator(secondItem).click();
+    // Product 2 selection
+
+    const secondItemLocator = 'a[href="prod.html?idp_=8"].hrefch';
+    const secondItemName = await newTab.locator(secondItemLocator).textContent();
+    console.log(secondItemName);
+    await newTab.locator(secondItemLocator).click();
     await newTab.locator(".btn.btn-success.btn-lg").click();
     await newTab.locator("[id='cartur']").click();
 
+    // Cart assertions
+
+    expect(await newTab.locator("[id='tbodyid']")).toContainText(firstItemName);
+    expect(await newTab.locator("[id='tbodyid']")).toContainText(secondItemName);
+    
     await page.waitForTimeout(5000);
 
 });
